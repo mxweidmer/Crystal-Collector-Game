f1ba9f6b2df4dee164b01dec28b03d183bee4e3e
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-02-27 19:48:42 -0800

    added basic html layout and declared initial variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+
+
+$(document).ready(function () {
+
+    var wins = 0;
+    var losses = 0;
+    var numToGuess;
+    var playerScore = 0;
+
+
+
+
+
+
+    $("").on("click", function () {
+
+
+    });
+
+});
