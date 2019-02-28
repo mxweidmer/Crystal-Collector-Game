3a4a62f4d643661948d6d6230af6c1d5a7de33f6
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-02-27 21:39:33 -0800

    nested images within buttons so .val worked, created function resetGame for reseting game after win or loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,17 +6,30 @@ $(document).ready(function () {
     var losses = 0;
     var playerScore = 0;
 
+    var blue = Math.floor(Math.random() * 12) + 1;
     var green = Math.floor(Math.random() * 12) + 1;
-    var green = Math.floor(Math.random() * 12) + 1;
-    var green = Math.floor(Math.random() * 12) + 1;
-    var green = Math.floor(Math.random() * 12) + 1;
+    var black = Math.floor(Math.random() * 12) + 1;
+    var white = Math.floor(Math.random() * 12) + 1;
 
     var numToGuess = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 
-    console.log(numToGuess);
+    $("#random-number").text(numToGuess);
 
+    function resetGame() {
+        blue = Math.floor(Math.random() * 12) + 1;
+        green = Math.floor(Math.random() * 12) + 1;
+        black = Math.floor(Math.random() * 12) + 1;
+        white = Math.floor(Math.random() * 12) + 1;
+
+        numToGuess = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+
+        $("#random-number").text(numToGuess);
+    }
 
     $(".crystal-image").on("click", function () {
+
+
+
 
 
     });
