2d8625a2802d7fdd700993203832a15481466e9b
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-02-27 20:08:18 -0800

    linked javascript file, wrote code for setting initial random values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,12 +4,16 @@ $(document).ready(function () {
 
     var wins = 0;
     var losses = 0;
-    var numToGuess;
     var playerScore = 0;
 
+    var green = Math.floor(Math.random() * 12) + 1;
+    var green = Math.floor(Math.random() * 12) + 1;
+    var green = Math.floor(Math.random() * 12) + 1;
+    var green = Math.floor(Math.random() * 12) + 1;
 
+    var numToGuess = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 
-
+    console.log(numToGuess);
 
 
     $("").on("click", function () {
